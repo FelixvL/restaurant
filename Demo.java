f696c940d6b78ccac60cfc65a8ba93483d2c819c
FelixvL <digitaleservice@gmail.com>
2019-05-02 15:22:42 +0200

    oop

--- Demo.java
+++ Demo.java
@@ -2,19 +2,69 @@ import java.util.Scanner;
 
 public class Demo {
 	public static void main (String[]args){
-		Klant pietje = new Klant();
+		Klant pietje = new Klant("Pietje");
 		RestaurantRuimte deSmikkel = new RestaurantRuimte();
 		Scanner sc = new Scanner(System.in);
+		deSmikkel.gastheerVraag(pietje);
+		
+		
+	}
+	
+
+}
+
+class Klant{
+	String naam;
+	Klant(String naam){
+		this.naam = naam;
+	}
+	void binnenkomen() {
+		System.out.println("Welkom nieuwe klant: "+naam);
+	}
+}
+
+class RestaurantRuimte{
+	int barkrukken = 2;
+	int tafels = 3;
+	Scanner sc = new Scanner(System.in);
+
+	
+	void bezoeken() {
+		System.out.println("1 voor bar 2 voor tafel");
+		int plek = sc.nextInt();
+		switch (plek) {
+		case 1:
+			if (barkrukken == 0) {
+				System.out.println("Er zijn geen barkrukken");
+			} else {
+				System.out.println("Klant gaat aan de bar zitten");
+				barkrukken--;
+			}
+			break;
+		case 2:
+			if (tafels == 0) {
+				System.out.println("Er zijn geen tafels");
+			} else {
+				System.out.println("Klant gaat aan de tafel zitten");
+				tafels--;
+			}
+			break;
+		default:
+			System.out.println("maak andere keuze");
+		}
+	}
+	
+	void gastheerVraag(Klant klant) {
 		boolean doorgaan = true;
 		while(doorgaan) {
 			System.out.println("Is er een klant?");
 			System.out.println("ja(j) nee(n) stoppen(s)");
-			String isErEenKlant = sc.nextLine();
+			String isErEenKlant = sc.next();
 			switch(  isErEenKlant.toLowerCase().charAt(0)  ) {
 			case 'j':
-				pietje.binnenkomen();
-				System.out.println("Er zijn "+ deSmikkel.barkrukken + " krukken beschikbaar!");
-				System.out.println("Er zijn "+ deSmikkel.tafels + " tafels beschikbaar! ");
+				klant.binnenkomen();
+				System.out.println("Er zijn "+ barkrukken + " krukken beschikbaar!");
+				System.out.println("Er zijn "+ tafels + " tafels beschikbaar! ");
 				bezoeken();
 				break;
 			case 'n':
@@ -26,41 +76,4 @@ public class Demo {
 		}
 		
 	}
-		static void bezoeken() {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("1 voor bar 2 voor tafel");
-			int plek = sc.nextInt();
-			switch(plek) {
-			case 1:
-				if(RestaurantRuimte.barkrukken == 0) {
-					System.out.println("Er zijn geen barkrukken");
-				}else {
-					System.out.println("Klant gaat aan de bar zitten");
-					RestaurantRuimte.barkrukken--;
-				}
-				break;
-			case 2:
-				if(RestaurantRuimte.tafels == 0) {
-					System.out.println("Er zijn geen tafels");
-				}else {
-					System.out.println("Klant gaat aan de tafel zitten");
-					RestaurantRuimte.tafels--;
-				}
-				break;
-			default: 
-				System.out.println("maak andere keuze");
-			}
-			
-		}
-	}
-
-class Klant{
-	void binnenkomen() {
-		System.out.println("Welkom nieuwe klant");
-	}
-}
-
-class RestaurantRuimte{
-	static int barkrukken = 2;
-	static int tafels = 3;
 }
