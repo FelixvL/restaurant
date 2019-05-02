b2816b04fe879bab10e59a43855cbd3dfd552145
FelixvL <digitaleservice@gmail.com>
2019-05-02 14:24:08 +0200

    derde restaurant object

--- Demo.java
+++ Demo.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Demo {
 	public static void main (String[]args){
 		Klant pietje = new Klant();
+		RestaurantRuimte deSmikkel = new RestaurantRuimte();
 		pietje.binnenkomen();
+		System.out.println("Er zijn "+ deSmikkel.barkrukken + " krukken beschikbaar!");
+		System.out.println("Er zijn "+ deSmikkel.tafels + " tafels beschikbaar! ");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("1 voor bar 2 voor tafel");
 		int invoer = sc.nextInt();
@@ -17,10 +20,16 @@ public class Demo {
 		default: 
 			System.out.println("maak andere keuze");
 		}
+		
 	}
 }
 class Klant{
 	void binnenkomen() {
 		System.out.println("Welkom nieuwe klant");
 	}
+}
+
+class RestaurantRuimte{
+	static int barkrukken = 8;
+	static int tafels = 10;
 }
