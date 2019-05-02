c44bd0c166322ca335a2a6cac45453dd2a3d0e41
FelixvL <digitaleservice@gmail.com>
2019-05-02 14:40:56 +0200

    dit wordt mijn bootschap

--- Demo.java
+++ Demo.java
@@ -4,25 +4,46 @@ public class Demo {
 	public static void main (String[]args){
 		Klant pietje = new Klant();
 		RestaurantRuimte deSmikkel = new RestaurantRuimte();
-		pietje.binnenkomen();
-		System.out.println("Er zijn "+ deSmikkel.barkrukken + " krukken beschikbaar!");
-		System.out.println("Er zijn "+ deSmikkel.tafels + " tafels beschikbaar! ");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("1 voor bar 2 voor tafel");
-		int invoer = sc.nextInt();
-		switch(invoer) {
-		case 1:
-			System.out.println("Klant gaat aan de bar zitten");
-			break;
-		case 2:
-			System.out.println("Klant gaat aan de tafel zitten");
-			break;
-		default: 
-			System.out.println("maak andere keuze");
+		boolean doorgaan = true;
+		while(doorgaan) {
+			System.out.println("Is er een klant?");
+			System.out.println("ja(j) nee(n) stoppen(s)");
+			String isErEenKlant = sc.nextLine();
+			switch(  isErEenKlant.toLowerCase().charAt(0)  ) {
+			case 'j':
+				pietje.binnenkomen();
+				System.out.println("Er zijn "+ deSmikkel.barkrukken + " krukken beschikbaar!");
+				System.out.println("Er zijn "+ deSmikkel.tafels + " tafels beschikbaar! ");
+				bezoeken();
+				break;
+			case 'n':
+				continue;
+			case 's':
+				doorgaan = false;
+			}
+			
 		}
 		
 	}
-}
+		static void bezoeken() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("1 voor bar 2 voor tafel");
+			int plek = sc.nextInt();
+			switch(plek) {
+			case 1:
+				System.out.println("Klant gaat aan de bar zitten");
+				break;
+			case 2:
+				System.out.println("Klant gaat aan de tafel zitten");
+				break;
+			default: 
+				System.out.println("maak andere keuze");
+			}
+			
+		}
+	}
+
 class Klant{
 	void binnenkomen() {
 		System.out.println("Welkom nieuwe klant");
